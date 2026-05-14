32d1b9b2b6ca511235c47838103b3e406b9eae79
Ashish Kumar <ashish9039062705@gmail.com>
2026-05-14 11:44:59 +0530

    feat: add express middleware and auth router setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser"
 
+dotenv.config();
 const app = express();
 
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+
+
 app.get('/', (req, res) => {
-  
+  res.send("💨server is running ")
 })
 
 export default app;
